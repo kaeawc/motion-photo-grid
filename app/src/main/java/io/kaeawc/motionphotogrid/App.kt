200cd7f4ec824e7c2d8ea642720c8bb8413359c1
Jason Pearson <jason.d.pearson@gmail.com>
2019-05-21 00:38:16 -0400

    added code style and logging

---
+++ app/src/main/java/io/kaeawc/motionphotogrid/App.kt
@@ -0,0 +1,13 @@
+package io.kaeawc.motionphotogrid
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
